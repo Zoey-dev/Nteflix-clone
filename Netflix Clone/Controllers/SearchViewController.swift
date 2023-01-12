03c669d1f818da69db0084c96a485fcb122ca8ca
Confidence Peter <confidence.peter.7@gmail.com>
2023-01-12 13:10:27 +0100

    Initial Commit

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Princess on 09/01/2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        
+    }
+    
+
+
+}
